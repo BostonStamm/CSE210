ee795fdb87dbdc2a8bd48443cb45120d107a9d49
Beansy2000 <75109494+Beansy2000@users.noreply.github.com>
2023-04-20 15:55:12 -0600

    Week 01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string Grade = Console.ReadLine();
+        int GradeInt = int.Parse(Grade);
+
+        string LetterGrade;
+
+        if(GradeInt >= 90){
+            LetterGrade = "A";
+        }else if(GradeInt >= 80){
+            LetterGrade = "B";
+        }else if(GradeInt >= 70){
+            LetterGrade = "C";
+        }else if(GradeInt >= 60){
+            LetterGrade = "D";
+        }else{
+            LetterGrade = "F";
+        }
+
+        if(GradeInt >= 60 && GradeInt < 93){
+            Int32 LastDigit = GradeInt % 10;
+            if(LastDigit >= 7 && LastDigit != 0){
+                LetterGrade = ($"{LetterGrade}+");
+            }else if(LastDigit < 3 && LastDigit != 0){
+                LetterGrade = ($"{LetterGrade}-");
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {GradeInt}({LetterGrade})");
+
+        if(GradeInt >= 70){
+            Console.Write("You Passed!");
+        }else{
+            Console.Write("You didn't pass. Don't get discouraged, and try again!");
+        }
     }
 }
